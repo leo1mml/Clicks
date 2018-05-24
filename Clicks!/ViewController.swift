b89bcb65df64ed5cafe69d386d284e309a0455ca
Leonel Menezes <mml_leo@hotmail.com>
2018-05-24 02:30:29 -0300

    initial commit

---
+++ Clicks!/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Clicks!
+//
+//  Created by Leonel Menezes on 21/05/2018.
+//  Copyright © 2018 Leonel Menezes. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
